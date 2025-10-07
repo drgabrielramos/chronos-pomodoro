c3260c501a8f917cb6eae8b8773b84114a301eff
Gabriel Ramos <cirurgiaoramos@gmail.com>
2025-10-07 08:43:31 -0300

    indicadores de ciclos

--- src/components/Mainform/index.tsx
+++ src/components/Mainform/index.tsx
@@ -75,9 +75,11 @@ export function MainForm () {
                     </p>
                 </div>
 
+                {state.currentCycle > 0 &&  (
                 <div className='formRow'>
                     <Cycles/>
                 </div>
+                )}
 
                 <div className='formRow'>
                     <DefaultButton icon = {<PlayCircleIcon/>}/>
